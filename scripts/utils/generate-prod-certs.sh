dd2500b6f31b797dcd6fa5bed155c3dd49956b33
Armando Galeano <armando@maeztra.com>
2025-06-07 13:08:51 -0300

    fix

--- scripts/utils/generate-prod-certs.sh
+++ scripts/utils/generate-prod-certs.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env sh
 set -e
 
+# Carrega as variáveis do .env
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+fi
+
+# Garante que a variável DOMAIN está definida
+if [ -z "$DOMAIN" ] || [ -z "$LETSENCRYPT_EMAIL" ]; then
+  echo "❌ As variáveis DOMAIN ou LETSENCRYPT_EMAIL não estão definidas."
+  exit 1
+fi
+
 # Só emite se NÃO existir o fullchain.pem
 if [ ! -f ./certs-production/live/${DOMAIN}/fullchain.pem ]; then
   docker run --rm \
